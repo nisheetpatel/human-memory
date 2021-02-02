1902fbd33d112f0633dfb9bff71215f7b8d6f883
nisheetpatel <nisheet.pat@gmail.com>
2021-02-02 14:39:39 +0100

    Added initial files

---
+++ dra_plotting_newest.py
@@ -0,0 +1,259 @@
+import numpy as np
+import pandas as pd
+from scripts.task import BottleneckTask
+from scripts.dra import DynamicResourceAllocator
+import multiprocessing as mp
+from time import time
+import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+# initializing hyperparameters
+n_restarts  = 5
+episodes    = 1000
+extraEps    = 501
+
+# Defining key states and actions for bottleneck task
+keyStates  = [5, 6, 16, 17, 27, 28]
+keyStates0 = [5, 16, 27]
+correctAct = [7, 9, 18, 20, 29, 31]
+keyActions = [7, 8, 9, 10, 18, 19, 20, 21, 29, 30, 31, 32]
+
+
+# Defining the function to be run in parallel across models
+def train_model(modelType, lmda=1, episodes=episodes,\
+    extraEps=extraEps, computeFreq=20, n_stages=3):
+    """
+    Trains model = {modelType} for {episodes} episodes
+    with stochastic choice sets, then for an additional
+    {extraEps} episodes without stochastic choice sets in
+    the Bottleneck task. Computes numerous things to plot
+    every {computeFreq} episodes and stores them in a
+    dataframe.
+
+    Returns the trained model that has the dataframe of
+    results stored in model.results.
+    """
+    
+    # Starting time counter
+    start = time()
+
+    # Splitting freqBased models into type and decay
+    mType = modelType
+    decay = 1
+    if modelType.split()[0] == 'freq':
+        mType = 'freqBased'
+        decay = float(modelType.split()[1])
+
+    model = DynamicResourceAllocator(model=mType, \
+        lmda=1, episodes=computeFreq, decay=decay, \
+        nGradUpdates=20, updateFreq=int(computeFreq/2),\
+        printFreq=1, printUpdates=False, n_stages=n_stages)
+
+    # initialising columns for dataframe 
+    col_model, col_episode, col_state, col_action, \
+    col_choiceAccuracy, col_pmistake, col_sigma, \
+    col_obj, col_expR, col_cost, \
+    col_nvisits, col_pvisit = [], [], [], [], [],\
+         [], [], [], [], [], [], []
+
+    old_nvisits = np.zeros(len(model.n_visits))
+
+    # Training model and collecting everything to plot
+    for episode in range(0, episodes+extraEps, \
+                    computeFreq):
+        """
+        Later on, check whether multiple entries in some
+        columns (e.g. cost, obj, expR) per episode lead
+        seaborn to plot narrower confidence intervals.
+        """
+        
+        # Compute expected reward, cost, and objective
+        cost = model.computeCost()
+        expR = model.computeExpectedReward(1000)
+        obj  = expR - cost
+
+        for state in keyStates:
+            for a in [0,1]:
+                # append everything to plot in cols
+                col_model.append(modelType)
+                col_episode.append(episode)
+                
+                col_state.append(state)
+
+                action = model.env.actions[state][a]
+                col_action.append(action)
+
+                idx = model.env.idx(state, action)
+                col_nvisits.append(model.n_visits[idx])
+                col_sigma.append(model.sigma[idx])
+
+                col_obj.append(obj)
+                col_expR.append(expR)
+                col_cost.append(cost)
+
+                # other action and its index
+                a1 = model.env.actions[state][1-a]
+                idx1 = model.env.idx(state, a1)
+                
+                # compute choice accuracy
+                chAc = model.n_visits[min(idx,idx1)]\
+                    / sum(model.n_visits[[idx,idx1]])
+                
+                col_choiceAccuracy.append(chAc)
+                
+                # compute prob. of making mistake
+                model.env.state = state
+                mistakes = 0
+                for _ in range(1000):
+                    a, _, _, _ = model.act(state)
+                    if a not in correctAct:
+                        mistakes += 1
+
+                col_pmistake.append(mistakes/1000)
+        
+        for state in keyStates0:
+            idx = model.env.idx(state, state+2)
+
+            n1 = np.sum( model.n_visits[idx:idx+2] ) -\
+                 np.sum( old_nvisits[idx:idx+2] )
+
+            n2 = np.sum( model.n_visits[idx+2:idx+4] ) -\
+                 np.sum( old_nvisits[idx+2:idx+4] )
+
+            col_pvisit.append(n1/(n1+n2+1e-5))
+            col_pvisit.append(n1/(n1+n2+1e-5))
+            col_pvisit.append(n2/(n1+n2+1e-5))
+            col_pvisit.append(n2/(n1+n2+1e-5))
+
+        # Store old n_visits to compute prop. visits
+        old_nvisits = model.n_visits.copy()
+
+        # See whether the environment needs to be switched
+        timeToSwitch = (episode >= episodes) & \
+            (episode < episodes+computeFreq)
+
+        if timeToSwitch:
+            # Changing env to remove stoch. choice sets
+            model.env = BottleneckTask(n_stages=n_stages,\
+                stochastic_choice_sets=False)
+        
+        # Train the model
+        model.train()
+
+    # End of training
+    # adding results to model object
+    model.results = pd.DataFrame({
+        'Model':            col_model,\
+        'Episode':          col_episode,\
+        'State':            col_state,\
+        'Action':           col_action,\
+        'Nvisits':          col_nvisits,\
+        'P_visit':          col_pvisit,\
+        'Sigma':            col_sigma,\
+        'Choice accuracy':  col_choiceAccuracy,\
+        'P_mistake':        col_pmistake,
+        'Objective':        col_obj,\
+        'Expected reward':  col_expR,\
+        'Cost':             col_cost        })
+
+    # Printing
+    timeTaken = str(datetime.timedelta\
+                (seconds=time()-start) )
+    print(f'Finished {modelType} in {timeTaken}.')
+
+    return model
+
+
+
+# Defining model types to train
+modelTypes = ['dra', 'equalPrecision', 'freq 1',\
+             'freq 0.9', 'freq 0.75', 'freq 0.5']
+
+# Running in parallel
+pool = mp.Pool()
+results = pool.map(train_model, \
+    np.repeat(modelTypes, n_restarts))
+
+
+# Concatenating all results in a mega dataframe
+for model in results:
+    if 'df' not in locals():
+        df = model.results
+    else:
+        df = pd.concat( [df, model.results], \
+            ignore_index=True, sort=False)
+
+# Path to save
+savePath = './figures/Rangel/bottleneckTask_newest/3_3/'
+
+# Plotting Objective, expected reward, cost
+for toPlot in ['Objective', 'Expected reward', 'Cost']:
+    fig, ax = plt.subplots()
+    ax = sns.lineplot(data=df, x="Episode", y=toPlot,\
+        hue="Model", style="Model", dashes=False)
+    plt.savefig(f'{savePath}Perf_{toPlot}')
+    plt.show()
+
+
+# Plotting evolution of observables
+for modelType in modelTypes:
+    
+    # Time updates
+    start = time()
+
+    # Extracting model's data
+    dfModel = df[df['Model']==modelType]
+
+    if modelType.split()[0] == 'freq':
+        saveName = 'freq' + str(int((float\
+            (modelType.split()[1])*100)))
+    else:
+        saveName = modelType.split()[0]
+
+    # Plotting p visit
+    fig, ax = plt.subplots()
+    ax = sns.lineplot(data=dfModel, x="Episode", \
+        y="P_visit", hue="State",\
+        legend='full', palette="Set2")
+    ax.set_title(f'{modelType}')
+    ax.set_ylim([0,1])
+    plt.savefig(f'{savePath}pvisit_{saveName}')
+    plt.show()
+
+    # Plotting sigma
+    fig, ax = plt.subplots()
+    ax = sns.lineplot(data=dfModel, x="Episode", \
+        y='Sigma', hue='Action', ci=None,\
+        legend='full', palette="Paired")
+    ax.set_title(f'{modelType}')
+    ax.set_ylim([0,55])
+    plt.savefig(f'{savePath}sigma_{saveName}')
+    plt.show()
+
+    # Plotting MA of P(mistake)
+    fig, ax = plt.subplots()
+    ax = sns.lineplot(data=dfModel, x="Episode", \
+        y="P_mistake", hue="State",\
+        legend='full', palette="Paired")
+    ax.set_title(f'{modelType}')
+    ax.set_ylim([0,0.65])
+    plt.savefig(f'{savePath}Obs_Pmistake_{saveName}')
+    plt.show()
+
+    # Plotting choice accuracy
+    fig, ax = plt.subplots()
+    ax = sns.lineplot(data=dfModel, x="Episode",\
+        y="Choice accuracy", hue="State",\
+        legend='full', palette="Paired")
+    ax.set_title(f'{modelType}')
+    ax.set_ylim([0.5,1])
+    plt.savefig(f'{savePath}Obs_choiceAccuracy_{saveName}')
+    plt.show()
+
+    # Printing time updates
+    print(f'Time to plot for {modelType} = '\
+     f'{str(datetime.timedelta(seconds=time()-start))}')
+
+plt.close('all')
---
+++ task.py
@@ -0,0 +1,397 @@
+import numpy as np
+import pandas as pd
+
+"""
+Defines the class environment as per Huys-Dayan-Rosier's planning task
+& the agent's long-term tabular memories: (s,a), r(s,a), Q(s,a), pi(a|s).
+"""
+class HuysTask:
+    # @transition_matrix:   State transition matrix
+    # @reward_matrix:       Rewards corresponding to state transitions
+    # @n_states:            Number of states
+    # @states:              State indices
+    def __init__(self, depth=3, n_states=6):
+        self.depth = depth
+        transitions = np.mat('0 1 0 1 0 0;\
+                            0 0 1 0 1 0;\
+                            0 0 0 1 0 1;\
+                            0 1 0 0 1 0;\
+                            1 0 0 0 0 1;\
+                            1 0 1 0 0 0')
+        rewards = np.mat('0   140  0   20  0   0; \
+                          0   0   -20  0  -70  0; \
+                          0   0    0  -20  0  -70;\
+                          0   20   0   0  -20  0; \
+                         -70  0    0   0   0  -20;\
+                         -20  0    20  0   0   0')
+
+        # Setting up the transitions and rewards matrices for the
+        # extended state space: 6 -> 6 x T_left
+        self.transition_matrix = np.zeros(((depth+1)*n_states,(depth+1)*n_states),dtype=int)
+        self.reward_matrix = np.zeros(((depth+1)*n_states,(depth+1)*n_states),dtype=int)
+
+        nrows = transitions.shape[0]
+        Nrows = self.transition_matrix.shape[0]
+
+        for i in range(nrows,Nrows,nrows):
+            self.transition_matrix[i-nrows:i,i:i+nrows] = transitions
+
+        for i in range(nrows,Nrows,nrows):
+            self.reward_matrix[i-nrows:i,i:i+nrows] = rewards
+
+        # Transitions, rewards, states
+        self.n_states = len(self.transition_matrix)
+        self.states = np.arange(self.n_states)
+        self.transitions = np.transpose(self.transition_matrix.nonzero())
+        self.rewards = np.array(self.reward_matrix[self.reward_matrix != 0])
+
+
+"""
+Custom-made two-step T-maze with 14 states.
+"""
+class Tmaze:
+    # @transition_matrix:   State transition matrix
+    # @reward_matrix:       Rewards corresponding to state transitions
+    # @n_states:            Number of states
+    # @states:              State indices
+    def __init__(self, depth=3, n_states=6, gridworld=False):
+        self.depth = depth
+        self.gridworld = gridworld
+        self.transition_matrix = \
+            np.mat('0 1 0 0 0 0 0 0 0 0 0 0 0 0;\
+                    1 0 1 1 0 0 0 0 0 0 0 0 0 0;\
+                    0 1 0 0 1 0 0 0 0 0 0 0 0 0;\
+                    0 1 0 0 0 1 0 0 0 0 0 0 0 0;\
+                    0 0 1 0 0 0 1 0 0 0 0 0 0 0;\
+                    0 0 0 1 0 0 0 1 0 0 0 0 0 0;\
+                    0 0 0 0 1 0 0 0 1 0 0 0 0 0;\
+                    0 0 0 0 0 1 0 0 0 1 0 0 0 0;\
+                    0 0 0 0 0 0 1 0 0 0 1 0 1 0;\
+                    0 0 0 0 0 0 0 1 0 0 0 1 0 1;\
+                    0 0 0 0 0 0 0 0 1 0 0 0 0 0;\
+                    0 0 0 0 0 0 0 0 0 1 0 0 0 0;\
+                    0 0 0 0 0 0 0 0 1 0 0 0 0 0;\
+                    0 0 0 0 0 0 0 0 0 1 0 0 0 0')
+        self.reward_matrix       = -self.transition_matrix
+        self.reward_matrix[8,10] =  10
+        self.reward_matrix[8,12] =  -5
+        self.reward_matrix[9,11] = -10
+        self.reward_matrix[9,13] =  5
+
+        # Transitions, rewards, states
+        self.n_states = len(self.transition_matrix)
+        self.states = np.arange(self.n_states)
+        self.transitions = np.transpose(self.transition_matrix.nonzero())
+        self.rewards = np.array(self.reward_matrix[self.reward_matrix != 0])
+
+
+""" 
+A wrapper class for a maze, containing all the information about the maze.
+Initialized to the 2D maze used by Mattar & Daw 2019 by default, however, 
+it can be easily adapted to any other maze by redefining obstacles and size.
+"""
+class Maze:
+    def __init__(self):
+        # maze width
+        self.WORLD_WIDTH = 9
+
+        # maze height
+        self.WORLD_HEIGHT = 6
+
+        # all possible actions
+        self.ACTION_UP = 0
+        self.ACTION_DOWN = 1
+        self.ACTION_LEFT = 2
+        self.ACTION_RIGHT = 3
+        self.actions = [self.ACTION_UP, self.ACTION_DOWN, self.ACTION_LEFT, self.ACTION_RIGHT]
+
+        # start state
+        self.START_STATE = [2, 0]
+
+        # goal state
+        self.GOAL_STATES = [[0, 8]]
+        self.new_goal = [[0, 6]]
+
+        # all obstacles
+        self.obstacles = [[1, 2], [2, 2], [3, 2], [0, 7], [1, 7], [2, 7], [4, 5]]
+        self.old_obstacles = [[0,7]]
+        self.new_obstacles = None
+
+        # time to change environment
+        self.switch_time = 3000
+
+        # initial state action pair values
+        # self.stateActionValues = np.zeros((self.WORLD_HEIGHT, self.WORLD_WIDTH, len(self.actions)))
+
+        # the size of q value
+        self.q_size = (self.WORLD_HEIGHT, self.WORLD_WIDTH, len(self.actions))
+
+        # max steps
+        self.max_steps = float('inf')
+
+        # track the resolution for this maze
+        self.resolution = 1
+
+        # Rangel task modification
+        self.TEMP_REWARD_STATES = [4,3]
+
+    # take @action in @state
+    # @return: [new state, reward]
+    def step(self, state, action):
+        x, y = state
+        if action == self.ACTION_UP:
+            x = max(x - 1, 0)
+        elif action == self.ACTION_DOWN:
+            x = min(x + 1, self.WORLD_HEIGHT - 1)
+        elif action == self.ACTION_LEFT:
+            y = max(y - 1, 0)
+        elif action == self.ACTION_RIGHT:
+            y = min(y + 1, self.WORLD_WIDTH - 1)
+        if [x, y] in self.obstacles:
+            x, y = state
+        if [x, y] in self.GOAL_STATES:
+            reward = 10
+        # elif [x, y] in [[4,3]]: #self.TEMP_REWARD_STATES:
+        #     reward = np.random.randn()
+        #     self.TEMP_REWARD_STATES = []
+        else:
+            reward = np.random.randn() -1
+        return [x, y], reward
+    
+    # Swith it up
+    def switch(self):
+        self.obstacles = [x for x in self.obstacles if x not in self.old_obstacles]
+        self.GOAL_STATES = self.new_goal
+        return
+
+
+""" 
+Modified mazes for experiments with Rangel.
+"""
+class RangelMaze:
+    def __init__(self, version=0, stochastic_rewards=True):
+        self.version = version
+        self.stochastic_rewards = stochastic_rewards
+
+        # maze width
+        self.WORLD_WIDTH = 8
+
+        # maze height
+        self.WORLD_HEIGHT = 7
+
+        # all possible actions
+        self.ACTION_UP = 0
+        self.ACTION_DOWN = 1
+        self.ACTION_LEFT = 2
+        self.ACTION_RIGHT = 3
+        self.actions = [self.ACTION_UP, self.ACTION_DOWN, self.ACTION_LEFT, self.ACTION_RIGHT]
+
+        if self.version==0:
+            # start state
+            self.START_STATE = [4, 3]
+
+            # goal state
+            self.GOAL_STATES = [[0, 5]]
+
+            # all obstacles
+            self.obstacles = [[4, 0], [1, 3], [2, 3], [4, 5]]
+
+            # positive reward states
+            self.rewarding_states = [[2,1], [5,1], [2,6], [5,6]]
+
+            # negative reward states
+            self.aversive_states = [[2,4], [3,6]]
+            
+        elif self.version==1:
+            self.START_STATE = [5, 2]
+
+            # goal state
+            self.GOAL_STATES = [[1, 5]]
+
+            # all obstacles
+            self.obstacles = [[2, 2], [3, 2], [4, 4], [4, 5]]
+
+            # positive reward states
+            self.rewarding_states = [[1,1], [5,6]]
+
+            # negative reward states
+            self.aversive_states = []
+
+        # the size of q value
+        self.q_size = (self.WORLD_HEIGHT, self.WORLD_WIDTH, len(self.actions))
+
+        # max steps
+        self.max_steps = float('inf')
+
+        # track the resolution for this maze
+        self.resolution = 1
+
+    # take @action in @state
+    # @return: [new state, reward]
+    def step(self, state, action):
+        x, y = state
+
+        # next state
+        if action == self.ACTION_UP:
+            x = max(x - 1, 0)
+        elif action == self.ACTION_DOWN:
+            x = min(x + 1, self.WORLD_HEIGHT - 1)
+        elif action == self.ACTION_LEFT:
+            y = max(y - 1, 0)
+        elif action == self.ACTION_RIGHT:
+            y = min(y + 1, self.WORLD_WIDTH - 1)
+        if [x, y] in self.obstacles:
+            x, y = state
+
+        # reward
+        if [x, y] in self.GOAL_STATES:
+            reward = 50
+        elif [x, y] in self.rewarding_states:
+            reward = 2
+        elif [x, y] in self.aversive_states:
+            reward = -3
+        else:
+            reward = -1
+        if self.stochastic_rewards:
+            reward += np.random.randn()
+
+        return [x, y], reward
+
+
+import random
+import scipy.linalg
+class BottleneckTask:
+    # @transition_matrix:   State transition matrix
+    # @reward_matrix:       Rewards corresponding to state transitions
+    # @n_states:            Number of states
+    # @states:              State indices
+    def __init__(self, n_stages=2, stochastic_rewards=True,\
+                stochastic_choice_sets=True):
+        self.start_state = 0
+        self.state = self.start_state
+        self.stochastic_rewards = stochastic_rewards
+        self.stochastic_choice_sets = stochastic_choice_sets
+        # self.transition_matrix = np.mat(\
+        #                      '0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1;\
+        #                       0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1')
+
+        # Defining one task stage
+        self.module = np.mat('1 1 1 1 0 0 0 0 0 0 0;\
+                              0 0 0 0 1 0 0 0 0 0 0;\
+                              0 0 0 0 1 0 0 0 0 0 0;\
+                              0 0 0 0 0 1 0 0 0 0 0;\
+                              0 0 0 0 0 1 0 0 0 0 0;\
+                              0 0 0 0 0 0 1 1 0 0 0;\
+                              0 0 0 0 0 0 0 0 1 1 0;\
+                              0 0 0 0 0 0 0 0 0 0 1;\
+                              0 0 0 0 0 0 0 0 0 0 1;\
+                              0 0 0 0 0 0 0 0 0 0 1;\
+                              0 0 0 0 0 0 0 0 0 0 1')
+
+        # Concatenating n_stages task stages
+        trMat = np.kron(np.eye(n_stages), self.module)
+
+        # Adding first and last states
+        self.transition_matrix = np.zeros(\
+                (trMat.shape[0]+1,trMat.shape[1]+1) )
+        self.transition_matrix[:-1,1:] = trMat
+        self.transition_matrix[-1,-1] = 1
+        
+        # Transitions, rewards, states
+        self.n_states = len(self.transition_matrix)
+        self.states = np.arange(self.n_states)
+        self.transitions = np.transpose(self.transition_matrix.nonzero())
+        
+        # Defining rewards
+        rewards1 = np.array([0, 140, 50, 100,  20, 0, 0, 20, -20, 20,  10])
+        rewards2 = np.array([0,  60,  0,  20, -20, 0, 0, 20, -20, 20, -20])
+        rewards3 = np.array([0, 140, 20, 100,  50, 0, 0, 20,  10, 20, -20])
+        if n_stages == 2:
+            self.rewards = np.hstack((rewards1, rewards2, 0))
+        elif n_stages == 3:
+            self.rewards = np.hstack((rewards1, rewards2, rewards3, 0))
+        else:
+            raise Exception('Current only supporting 2-3 stages')
+
+        # actions available from each state
+        self.actions = [list(self.transition_matrix[ii].nonzero()[0]) \
+                        for ii in range(len(self.transition_matrix))]
+
+        # the size of q value
+        self.q_size = len(self.transitions)
+
+
+    def reset(self):
+        self.state = self.start_state
+        return self.start_state
+
+    # @property decorator 
+    @property
+    def action_space(self):
+        assert self.state <= self.n_states
+        if self.stochastic_choice_sets:
+            if self.state not in [0,11]:
+                return self.actions[self.state]
+
+            elif self.state == 0:
+                # Optimal agent visits states 5 or 6 with prob 0.5 & 0.5
+                choiceList = [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
+                return random.choices(choiceList, weights=(1,1,1,2,1,2), k=1)[0]
+
+            elif self.state == 11:
+                # Optimal agent visits states 16 or 17 with prob 0.8 and 0.2
+                choiceList = [[12,13], [12,14], [12,15], [13,14], [13,15], [14,15]]
+                return random.choices(choiceList, weights=(2,2,2,1,2,1), k=1)[0]
+
+            elif self.state == 22:
+                # Optimal agent visits states 16 or 17 with prob 0.8 and 0.2
+                choiceList = [[23,24], [23,25], [23,26], [24,25], [24,26], [25,26]]
+                return random.choices(choiceList, weights=(2,2,2,1,2,1), k=1)[0]
+        else:
+            return self.actions[self.state]
+
+    # point to location in q-table
+    def idx(self, state, action):
+        ii  = np.logical_and(self.transitions[:,0]==state,\
+                    self.transitions[:,1]==action)
+        return int(np.argwhere(ii))
+
+    # step in the environment
+    def step(self, action):
+        # next state
+        next_state = action
+        self.state = next_state
+
+        # reward
+        reward = self.rewards[next_state]
+        if self.stochastic_rewards:
+            reward += np.random.randn()
+
+        # termination and info/logs
+        done = False
+        if next_state == self.n_states-1:
+            done = True
+        info = None
+
+        return next_state, reward, done, info
