06d76e4744dbe2dcf4be3b2d21b06824ea4dc739
Nisheet Patel <nisheet.pat@gmail.com>
2021-02-12 12:59:47 +0100

    restructured tasks and added versions

--- task.py
+++ task.py
@@ -1,5 +1,144 @@
 import numpy as np
 import pandas as pd
+import random
+import scipy.linalg
+
+class BottleneckTask:
+    # @transition_matrix:   State transition matrix
+    # @reward_matrix:       Rewards corresponding to state transitions
+    # @n_states:            Number of states
+    # @states:              State indices
+    def __init__(self, n_stages=2, stochastic_rewards=True,\
+                stochastic_choice_sets=True, version=1):
+        self.start_state = 0
+        self.state = self.start_state
+        self.stochastic_rewards = stochastic_rewards
+        self.stochastic_choice_sets = stochastic_choice_sets
+
+        # Defining one task stage
+        self.module = np.mat('1 1 1 1 0 0 0 0 0 0 0;\
+                              0 0 0 0 1 0 0 0 0 0 0;\
+                              0 0 0 0 1 0 0 0 0 0 0;\
+                              0 0 0 0 0 1 0 0 0 0 0;\
+                              0 0 0 0 0 1 0 0 0 0 0;\
+                              0 0 0 0 0 0 1 1 0 0 0;\
+                              0 0 0 0 0 0 0 0 1 1 0;\
+                              0 0 0 0 0 0 0 0 0 0 1;\
+                              0 0 0 0 0 0 0 0 0 0 1;\
+                              0 0 0 0 0 0 0 0 0 0 1;\
+                              0 0 0 0 0 0 0 0 0 0 1')
+
+        # Concatenating n_stages task stages
+        trMat = np.kron(np.eye(n_stages), self.module)
+
+        # Adding first and last states
+        self.transition_matrix = np.zeros(\
+                (trMat.shape[0]+1,trMat.shape[1]+1) )
+        self.transition_matrix[:-1,1:] = trMat
+        self.transition_matrix[-1,-1] = 1
+        
+        # Transitions, rewards, states
+        self.n_states = len(self.transition_matrix)
+        self.states = np.arange(self.n_states)
+        self.transitions = np.transpose(self.transition_matrix.nonzero())
+        
+        # Defining rewards
+        rewards1 = np.array([0, 140, 50, 100,  20, 0, 0, 20, -20, 20,   0])
+        rewards2 = np.array([0,  60,  0,  20, -20, 0, 0, 20,   0, 20, -20])
+        rewards3 = np.array([0, 140, 40, 100,  70, 0, 0, 20,  10, 20, -10])
+
+        if version==2:
+            rewards2 = np.array([0,  60,  0,  20, -20, 0, 0, 20, -5, 20, -20])
+            rewards3 = np.array([0, 140, 50,  100, 20, 0, 0, 20, 10, 20, -10])
+
+        if n_stages == 2:
+            self.rewards = np.hstack((rewards1, rewards2, 0))
+
+        elif n_stages == 3:
+            self.rewards = np.hstack((rewards1, rewards2, rewards3, 0))
+
+        else:
+            raise Exception('Current only supporting 2-3 stages')
+
+        # actions available from each state
+        self.actions = [list(self.transition_matrix[ii].nonzero()[0]) \
+                        for ii in range(len(self.transition_matrix))]
+
+        # the size of q value
+        self.q_size = len(self.transitions)
+
+
+    def reset(self):
+        self.state = self.start_state
+        return self.start_state
+
+
+    @property
+    def action_space(self):
+        assert self.state <= self.n_states
+        
+        if self.stochastic_choice_sets:
+
+            if np.mod(self.state,11) != 0:
+                # Bottleneck states are 0, 11, 22
+                return self.actions[self.state]
+
+            elif self.state == 0:
+                # Optimal agent visits states 5 or 6 with prob 0.5 & 0.5
+                choiceList = [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
+                return random.choices(choiceList, weights=(1,1,1,2,1,2), k=1)[0]
+
+            elif self.state == 11:
+                # Optimal agent visits states 16 or 17 with prob 0.8 and 0.2
+                choiceList = [[12,13], [12,14], [12,15], [13,14], [13,15], [14,15]]
+                return random.choices(choiceList, weights=(2,2,2,1,2,1), k=1)[0]
+
+            elif self.state == 22:
+                # Optimal agent visits states 16 or 17 with prob 0.8 and 0.2
+                choiceList = [[23,24], [23,25], [23,26], [24,25], [24,26], [25,26]]
+                return random.choices(choiceList, weights=(2,2,2,1,2,1), k=1)[0]
+        else:
+            return self.actions[self.state]
+
+    # point to location in q-table
+    def idx(self, state, action):
+        ii  = np.logical_and(self.transitions[:,0]==state,\
+                    self.transitions[:,1]==action)
+        return int(np.argwhere(ii))
+
+    # step in the environment
+    def step(self, action):
+        # next state
+        next_state = action
+        self.state = next_state
+
+        # reward
+        reward = self.rewards[next_state]
+        if self.stochastic_rewards:
+            reward += np.random.randn()
+
+        # termination and info/logs
+        done = False
+        if next_state == self.n_states-1:
+            done = True
+        info = None
+
+        return next_state, reward, done, info
+
+
+
+#########################################################################
+#                                                                       #
+# Other tasks that we tried running DRA on include the planning task    #
+# from Huys et al 2015, a T-maze, a wrapper for arbitrary 2D gridwords  #
+# which we used to create the Mattar & Daw 2019 maze, and a couple of   #
+# mazes we tried for the human experiments with Antonio Rangel but      #
+# eventually gave up on. Though they are not being used in the current  #
+# experiments, they live below. In order to be used with the current    #
+# DynamicResourceAllocator object, they need to be modified slightly    #
+# to have the openAI gym-like structure as in the BottleneckTask above. #
+#                                                                       #
+#########################################################################
 
 """
 Defines the class environment as per Huys-Dayan-Rosier's planning task
@@ -256,118 +395,3 @@ class RangelMaze:
             reward += np.random.randn()
 
         return [x, y], reward
-
-
-import random
-import scipy.linalg
-class BottleneckTask:
-    # @transition_matrix:   State transition matrix
-    # @reward_matrix:       Rewards corresponding to state transitions
-    # @n_states:            Number of states
-    # @states:              State indices
-    def __init__(self, n_stages=2, stochastic_rewards=True,\
-                stochastic_choice_sets=True):
-        self.start_state = 0
-        self.state = self.start_state
-        self.stochastic_rewards = stochastic_rewards
-        self.stochastic_choice_sets = stochastic_choice_sets
-
-        # Defining one task stage
-        self.module = np.mat('1 1 1 1 0 0 0 0 0 0 0;\
-                              0 0 0 0 1 0 0 0 0 0 0;\
-                              0 0 0 0 1 0 0 0 0 0 0;\
-                              0 0 0 0 0 1 0 0 0 0 0;\
-                              0 0 0 0 0 1 0 0 0 0 0;\
-                              0 0 0 0 0 0 1 1 0 0 0;\
-                              0 0 0 0 0 0 0 0 1 1 0;\
-                              0 0 0 0 0 0 0 0 0 0 1;\
-                              0 0 0 0 0 0 0 0 0 0 1;\
-                              0 0 0 0 0 0 0 0 0 0 1;\
-                              0 0 0 0 0 0 0 0 0 0 1')
-
-        # Concatenating n_stages task stages
-        trMat = np.kron(np.eye(n_stages), self.module)
-
-        # Adding first and last states
-        self.transition_matrix = np.zeros(\
-                (trMat.shape[0]+1,trMat.shape[1]+1) )
-        self.transition_matrix[:-1,1:] = trMat
-        self.transition_matrix[-1,-1] = 1
-        
-        # Transitions, rewards, states
-        self.n_states = len(self.transition_matrix)
-        self.states = np.arange(self.n_states)
-        self.transitions = np.transpose(self.transition_matrix.nonzero())
-        
-        # Defining rewards
-        rewards1 = np.array([0, 140, 50, 100,  20, 0, 0, 20, -20, 20,  10])
-        rewards2 = np.array([0,  60,  0,  20, -20, 0, 0, 20, -20, 20, -20])
-        rewards3 = np.array([0, 140, 20, 100,  50, 0, 0, 20,  10, 20, -20])
-        if n_stages == 2:
-            self.rewards = np.hstack((rewards1, rewards2, 0))
-        elif n_stages == 3:
-            self.rewards = np.hstack((rewards1, rewards2, rewards3, 0))
-        else:
-            raise Exception('Current only supporting 2-3 stages')
-
-        # actions available from each state
-        self.actions = [list(self.transition_matrix[ii].nonzero()[0]) \
-                        for ii in range(len(self.transition_matrix))]
-
-        # the size of q value
-        self.q_size = len(self.transitions)
-
-
-    def reset(self):
-        self.state = self.start_state
-        return self.start_state
-
-    # @property decorator 
-    @property
-    def action_space(self):
-        assert self.state <= self.n_states
-        if self.stochastic_choice_sets:
-            if self.state not in [0,11]:
-                return self.actions[self.state]
-
-            elif self.state == 0:
-                # Optimal agent visits states 5 or 6 with prob 0.5 & 0.5
-                choiceList = [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
-                return random.choices(choiceList, weights=(1,1,1,2,1,2), k=1)[0]
-
-            elif self.state == 11:
-                # Optimal agent visits states 16 or 17 with prob 0.8 and 0.2
-                choiceList = [[12,13], [12,14], [12,15], [13,14], [13,15], [14,15]]
-                return random.choices(choiceList, weights=(2,2,2,1,2,1), k=1)[0]
-
-            elif self.state == 22:
-                # Optimal agent visits states 16 or 17 with prob 0.8 and 0.2
-                choiceList = [[23,24], [23,25], [23,26], [24,25], [24,26], [25,26]]
-                return random.choices(choiceList, weights=(2,2,2,1,2,1), k=1)[0]
-        else:
-            return self.actions[self.state]
-
-    # point to location in q-table
-    def idx(self, state, action):
-        ii  = np.logical_and(self.transitions[:,0]==state,\
-                    self.transitions[:,1]==action)
-        return int(np.argwhere(ii))
-
-    # step in the environment
-    def step(self, action):
-        # next state
-        next_state = action
-        self.state = next_state
-
-        # reward
-        reward = self.rewards[next_state]
-        if self.stochastic_rewards:
-            reward += np.random.randn()
-
-        # termination and info/logs
-        done = False
-        if next_state == self.n_states-1:
-            done = True
-        info = None
-
-        return next_state, reward, done, info
