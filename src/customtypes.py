1dc9b362ab204b73cb97df414aeb0498ff48b02d
nisheetpatel <nisheet.pat@gmail.com>
2022-05-20 09:36:57 +0200

    updated experience to be a dict instead of tuple

--- src/customtypes.py
+++ src/customtypes.py
@@ -1,7 +1,17 @@
-# defining type aliases
+from typing import TypedDict
+
+# custom type aliases
 Action = int
 ActionSpace = list[int, int]
 State = int
 Reward = float
 Done = bool
 Info = dict
+
+
+class Experience(TypedDict):
+    state: State
+    action: Action
+    reward: Reward
+    next_state: State
+    prob_actions: list[float]
