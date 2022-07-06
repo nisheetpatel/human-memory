f5122de297f3681973d491915bf60414b2d8c4f7
nisheetpatel <nisheet.pat@gmail.com>
2022-07-06 15:56:47 +0200

    adapted agents to be instantiated with make() method

--- src/main.py
+++ src/main.py
@@ -5,18 +5,28 @@ from simulator import Simulator
 from tasks import Memory2AFC
 
 
-def train_dra() -> float:
+def train_dra() -> None:
     env = Memory2AFC()
-    agent = DRA(q_size = env.n_states)
+    agent = DRA(size=env.n_states).make()
     simulator = Simulator(agent, env)
-    reward = simulator.run_episode()
-    print(f"Reward obtained = {round(reward,2)}")
-    return reward
+
+    print(f"Training {agent.model.value}")
+
+    for _ in range(1770):
+        simulator.run_episode()
+
+    print("\nFinished training!\n")
+
 
 def train_all_models() -> None:
     envs = [Memory2AFC() for _ in range(4)]
-    q_size = envs[0].n_states
-    agents = [DRA(q_size), FreqRA(q_size), StakesRA(q_size), EqualRA(q_size)]
+    size = envs[0].n_states
+    agents = [
+        DRA(size).make(),
+        FreqRA(size).make(),
+        StakesRA(size).make(),
+        EqualRA(size).make(),
+    ]
     simulators = [Simulator(agent, env) for agent, env in zip(agents, envs)]
 
     for simulator in simulators:
