211008cd2ddad077cdf723bd9438997937a35ffb
nisheetpatel <nisheet.pat@gmail.com>
2024-02-19 20:25:01 +0100

    move scripts from src/ to here; clean up later

---
+++ src/scripts_old/analysis_script.py
@@ -0,0 +1,49 @@
+from definitions import DATA_PATH, FIGURE_PATH, MODEL_PATH
+from src.analysis.hierarchical.classifier import Classifier
+from src.analysis.hierarchical.model import HierarchicalModel
+from src.analysis.hierarchical.plotter import Plotter
+from src.data.processor import DataProcessor
+
+# set parameters for fitting hierarchical model
+hierarchical_model_params = {"n_chains": 4, "n_samples": 25_000}
+
+# set parameters for classifier
+classifier_params = {"equality_thresh": 1.7, "class_thresh": 20}
+
+# read and process data, then extract performance metrics
+data_processor = DataProcessor(path=DATA_PATH + "pilot_slot-machines_3/")
+df = data_processor.get_processed_data()
+# df_good = df.loc[df["above_chance"]]
+df_all = df
+perf = data_processor.extract_performance_metrics(df)
+
+# fit hierarchical model to good subjects
+model = HierarchicalModel()
+choice_data_dict = model.get_choice_data_dict(df_all)
+fit = model.fit_posterior(choice_data_dict, **hierarchical_model_params)
+model.save(fit, f"{MODEL_PATH}all_subjects.pkl")
+
+# classify subjects
+classifier = Classifier(fit, **classifier_params)
+df_class = classifier.classify()
+perf = classifier.merge_with_performance_metrics(perf)
+
+# plot posterior at individual level
+plotter = Plotter(fit, save_path=FIGURE_PATH)
+plotter.plot_posterior_over_betas_individual("betas-individual_all-subjects", perf)
+plotter.plot_posteriors_at_group_level("betas-group_all-subjects")
+
+# split participants by their assigned class
+model_names = ["DRA", "Freq", "Stakes", "EP"]
+ids = [classifier.get_ids(model_name) for model_name in model_names]
+dfs = [df_all.loc[df_all["id"].isin(ids_model)].copy() for ids_model in ids]
+
+# fit hierarchical model to good subjects of each class
+fits = []
+for i, (df, model_name) in enumerate(zip(dfs, model_names)):
+    print(f"Fitting model {model_name} of {len(dfs)} subjects")
+    model = HierarchicalModel()
+    choice_data_dict = model.get_choice_data_dict(df)
+    fit = model.fit_posterior(choice_data_dict, **hierarchical_model_params)
+    fits.append(fit)
+    model.save(fit, f"{MODEL_PATH}{model_name}.pkl")
